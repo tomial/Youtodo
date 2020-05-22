1f9be06c60ae35c23c34cdaa16e007b844b72b61
tomial <b1250284002@hotmail.com>
2020-05-22 10:49:46 +0800

    initialize server project

---
+++ youtodo-server/src/main/java/com/tomial/youtodoserver/YoutodoServerApplication.java
@@ -0,0 +1,13 @@
+package com.tomial.youtodoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutodoServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(YoutodoServerApplication.class, args);
+  }
+
+}
---
+++ youtodo-server/src/test/java/com/tomial/youtodoserver/YoutodoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomial.youtodoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutodoServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
